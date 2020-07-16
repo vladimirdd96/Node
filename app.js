fae1a94901508ee6af658e6edc1df7a6aa169c53
banea9 <banea9@gmail.com>
2020-07-16 20:01:16 +0300

    adding mongo

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Book = require('./Book.model');
 const port = 8080;
-const db = 'mongodb://localhost/example';  // Using a database called example
+const MONDODB_URI = 'mongodb://localhost/example';  // Using a database called example
 
-mongoose.mongo.connect(db,
-    { useUnifiedTopology: true }            // I defined that on connect cause of some type of error with the mongodb constructor 
-);
 
 app.get('/', function (req, res) {
     res.send('happy to be here')            //Simple response from the database just to check if it all work
@@ -28,9 +25,11 @@ app.get('/books', function (req, res) {
 });
 
 
-
-
-app.listen(port, function () {
-    console.log('app listening on port: ' + port);
-});
+mongoose.connect(MONDODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Connected to DB")
+    app.listen(8080)
+}).catch(err => console.log(err))
 
