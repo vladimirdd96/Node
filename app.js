c3c2c780c06038aba41df0adcd7caebfa84187a8
Vladimir <vladimirdd96@gmail.com>
2020-07-17 16:57:52 +0300

    fix

--- app.js
+++ app.js
@@ -76,20 +76,24 @@ app.post('/book2', function (req, res) {   // setting up the route and what to d
 });
 
 app.put('/book/:id', function (req, res) {      //findOneAndUpdate method inside of an api call to /book/:id
-    Book.findOneAndUpdate({         //looks like findOne book but we update it also
-        _id: req.params.id      // setting up the query
-    }, {
+    Book.findOneAndUpdate(
+        {                        //looks like findOne book but we update it also
+            _id: req.params.id      // setting up the query
+        }, {
         $set:
-            { title: req.body.title }
+        {
+            title: req.body.title,
+            author: req.body.author
+        }
     },        //setting the title that we want to change
-        { upsert: true },
+        { upsert: true },  //let mongoose know if title dont alrready exist to insert it
         {
             function(err, newBook) {
                 if (err) {
                     console.log('error occured');
                 } else {
                     console.log(newBook);
-                    res.status(204);          // after the update i put status 204 just so i know i updated correctly
+                    res.send(newBook);          // after the update send me back the newBook i updated
                 }
             }
         })
